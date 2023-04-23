7f4e5e357629caf29f35a5f8be448508a60539f3
crown-of-napoleon <112432640+crown-of-napoleon@users.noreply.github.com>
2023-04-23 13:25:32 -0500

    Create DBSCAN.py

---
+++ AnomalyDetection/DBSCAN.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+
+# Load the data and preprocess it
+data = pd.read_csv('output.csv')
+X = data.values
+X = StandardScaler().fit_transform(X)
+
+# Apply DBSCAN
+dbscan = DBSCAN(eps=0.5, min_samples=10)
+dbscan.fit(X)
+
+# Get the indices of the outliers
+outliers_mask = np.zeros_like(dbscan.labels_, dtype=bool)
+outliers_mask[dbscan.labels_ == -1] = True
+outliers_indices = np.where(outliers_mask)[0]
+
+# Print the indices of the outliers
+print("Outlier indices:", outliers_indices)
