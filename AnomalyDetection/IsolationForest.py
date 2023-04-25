7d6b549635a259eaca0d9a1fdc9f3af8b093a0a8
crown-of-napoleon <112432640+crown-of-napoleon@users.noreply.github.com>
2023-04-24 23:20:23 -0500

    Create IsolationForest.py

---
+++ AnomalyDetection/IsolationForest.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+
+# Load the data and preprocess it
+data = pd.read_csv('output.csv')
+X = data.values
+X = StandardScaler().fit_transform(X)
+
+# Apply Isolation Forest
+isolation_forest = IsolationForest(n_estimators=100, contamination=0.01, random_state=42)
+isolation_forest.fit(X)
+
+# Get the indices of the outliers
+outliers_mask = isolation_forest.predict(X) == -1
+outliers_indices = np.where(outliers_mask)[0]
+
+# Print the indices of the outliers
+print("Outlier indices:", outliers_indices)
