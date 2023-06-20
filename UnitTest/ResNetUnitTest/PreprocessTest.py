526519409460ec2c5f2113a64f0941baee9dca2a
crown-of-napoleon <112432640+crown-of-napoleon@users.noreply.github.com>
2023-06-20 20:42:04 +0800

    Create PreprocessTest.py

---
+++ UnitTest/ResNetUnitTest/PreprocessTest.py
@@ -0,0 +1,9 @@
+class TestDataPreprocessing(unittest.TestCase):
+    def test_preprocess_data(self):
+        from preprocess_data import preprocess_data
+        (x_train, y_train), _ = preprocess_data()
+        expected_train_size = 1000  # replace with your expected train size
+        self.assertEqual(x_train.shape[0], expected_train_size)
+        self.assertTrue(np.all((0 <= x_train) & (x_train <= 1)))
+
+unittest.main()
