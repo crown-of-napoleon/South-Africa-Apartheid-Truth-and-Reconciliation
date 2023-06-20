e77d62fabec407931e1ebcd5e585f66f6e8a5c90
crown-of-napoleon <112432640+crown-of-napoleon@users.noreply.github.com>
2023-06-20 20:36:24 +0800

    Create PreprocessTest.py

---
+++ UnitTest/PreprocessTest.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+
+class TestDataPreprocessing(unittest.TestCase):
+    def test_preprocess_data(self):
+        from preprocess_data import preprocess_data
+        (x_train, y_train), _ = preprocess_data()
+        expected_train_size = 1374
+        self.assertEqual(x_train.shape[0], expected_train_size)
+        self.assertTrue(np.all((0 <= x_train) & (x_train <= 1)))
+
+unittest.main()
